1fac92f8cda0fe539eb5db9b6dc1ca4ca4f44cc4
Yakov <doombringer_2006@mail.ru>
2019-03-01 02:48:02 +0300

    +

---
+++ DZ1H/script.js
@@ -0,0 +1,9 @@
+let num = "33721",
+    proizved = num[0] * num [1] * num[2] * num[3] * num[4];
+
+    console.log(proizved);
+
+let stenen = String(proizved ** 3); 
+   
+
+alert(stenen[0] + stenen[1]);
