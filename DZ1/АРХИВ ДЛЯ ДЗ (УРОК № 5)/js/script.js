bdc4a194f94d3e93fe476fefb263b80a955f6191
Yakov <doombringer_2006@mail.ru>
2019-03-12 15:14:37 +0300

    -

--- DZ1/АРХИВ ДЛЯ ДЗ (УРОК № 5)/js/script.js
+++ DZ1/АРХИВ ДЛЯ ДЗ (УРОК № 5)/js/script.js
@@ -5,16 +5,21 @@ let menuItem = document.querySelectorAll('.menu-item'),
     adv = document.getElementsByClassName("adv"),
     answer = document.getElementById("prompt");
 
+    // second = document.querySelectorAll(".menu-item[1]"),
+    // third = document.querySelectorAll(".menu-item[2]");
+    // console.log(second, third);
+
 newElement.classList.add("menu-item");
 menu.appendChild(newElement);
 newElement.textContent = "Пятый пункт";
-// menu.replaceChild("menuItem[1], menuItem[2]");
+// menu.insertBefore("menuItem[1], menuItem[2]");
 document.body.style.background = "url(img/apple_true.jpg) center no-repeat"; 
 title.innerHTML = ("Мы продаем только <strong>подлинную</strong> технику Apple");
 answer.textContent = prompt("Че ваще?");
+document.body.removeChild("adv");
 
 
 // console.log(menuItem);
 // console.log(newElement);
 
-// menu.insertBefore(newElement, menu[3]);
+menu.insertBefore(newElement, menu[3]);
