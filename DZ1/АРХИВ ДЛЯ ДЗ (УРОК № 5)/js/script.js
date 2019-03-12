c6e88da86449e0183f7cd02d79885823ef8d4b65
Yakov <doombringer_2006@mail.ru>
2019-03-12 21:37:39 +0300

    +

--- DZ1/АРХИВ ДЛЯ ДЗ (УРОК № 5)/js/script.js
+++ DZ1/АРХИВ ДЛЯ ДЗ (УРОК № 5)/js/script.js
@@ -2,24 +2,15 @@ let menuItem = document.querySelectorAll('.menu-item'),
     menu = document.querySelector(".menu"),
     newElement = document.createElement('li'),
     title = document.getElementById("title"),
-    adv = document.getElementsByClassName("adv"),
+    adv = document.getElementsByClassName("adv")[0],
     answer = document.getElementById("prompt");
 
-    // second = document.querySelectorAll(".menu-item[1]"),
-    // third = document.querySelectorAll(".menu-item[2]");
-    // console.log(second, third);
 
 newElement.classList.add("menu-item");
 menu.appendChild(newElement);
 newElement.textContent = "Пятый пункт";
-// menu.insertBefore("menuItem[1], menuItem[2]");
+menu.insertBefore(menuItem[2], menuItem[1]);
 document.body.style.background = "url(img/apple_true.jpg) center no-repeat"; 
 title.innerHTML = ("Мы продаем только <strong>подлинную</strong> технику Apple");
 answer.textContent = prompt("Че ваще?");
-document.body.removeChild("adv");
-
-
-// console.log(menuItem);
-// console.log(newElement);
-
-menu.insertBefore(newElement, menu[3]);
+adv.remove(); 
